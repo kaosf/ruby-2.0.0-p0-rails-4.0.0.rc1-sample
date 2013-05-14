7a02ce5908a5dc6a8ed051d38de5f62868feb71e
ka <ka.kaosf@gmail.com>
2013-05-14 15:14:41 +0900

    init
    execute 'bundle exec rails new sample'
    
    versions
     ruby 2.0.0-p0
     bundler 1.3.5
     rails 4.0.0.rc1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sample::Application.config.secret_key_base = 'b31de8a5f055c705597e8cba3a6d31c930d10189433ba29a2a9d05f94ff335850b8c5e8c11cead78e4b52b66b53593f36b84fae78c3d9ecc456ac976e91be1f3'
